070e35f9f3c04eaea5f23d05a7701b85cfd97ea9
VyraWasHere <joakimoks@gmail.com>
2017-11-08 01:19:17 +0100

    Fix: Verification system expects "/exchange", app expected "/exchange/"

--- cmd/assignment2/main.go
+++ cmd/assignment2/main.go
@@ -7,39 +7,49 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/exchange/", exchangeHandler)
+	http.HandleFunc("/exchange", exchangeHandler)
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
 
 func exchangeHandler(w http.ResponseWriter, r *http.Request) {
 	parts := strings.Split(r.URL.Path, "/")
 
-	switch parts[2] {
-
-	case "avarage":
-		avgHandler(w, r)
-
-	case "latest":
-		rateHandler(w, r)
-
-	case "evaluationtrigger":
-		evalHandler(w, r)
-
-	case "":
+	if len(parts) == 2 {
 		if r.Method == http.MethodPost {
 			webhookHandler(w, r)
 		} else {
 			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 			return
 		}
+	} else if len(parts) > 2 {
+		switch parts[2] {
 
-	default:
-		if r.Method == http.MethodGet || r.Method == http.MethodDelete {
-			webhookHandler(w, r)
-		} else {
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-			return
+		case "avarage":
+			avgHandler(w, r)
+
+		case "latest":
+			rateHandler(w, r)
+
+		case "evaluationtrigger":
+			evalHandler(w, r)
+
+		case "":
+			if r.Method == http.MethodPost {
+				webhookHandler(w, r)
+			} else {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+
+		default:
+			if r.Method == http.MethodGet || r.Method == http.MethodDelete {
+				webhookHandler(w, r)
+			} else {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
 		}
+	} else {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 	}
-
 }
